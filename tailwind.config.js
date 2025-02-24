e2c7052ceee1fb60ffd3e57fd973e9b5eba61ea5
Archie Cayabyab <archiejosephcayabyab@gmail.com>
2025-02-24 23:10:08 +0800

    feat: Installed Tailwind, ReactIcons, and Framer  Motion

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+
+import { DEFAULT } from '@react-three/fiber/dist/declarations/src/core/utils';
+
+export default {
+    content: [
+        "./index.html",
+        "./src/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme: {
+        extend: {
+            colors: {
+                black: "#08090A",
+                gray: {
+                    DEFAULT: "#737373"
+                }
+            }
+        },
+    },
+    plugins: [],
+}
+
